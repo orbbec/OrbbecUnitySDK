d9210cd96b367fc1d2a7a785c665de1cd2bf604d
wuhao <wuhao@orbbec.com>
2022-09-23 16:11:03 +0800

    [update] upgrade dll to 1.4.x

--- Assets/Orbbec/Scripts/GlobalDefine.cs
+++
@@ -1,9 +0,0 @@
-public static class GlobalDefine
-{
-    public static int COLOR_WIDTH = 640;
-    public static int COLOR_HEIGHT = 480;
-    public static int COLOR_FPS = 30;
-    public static int DEPTH_WIDTH = 640;
-    public static int DEPTH_HEIGHT = 480;
-    public static int DEPTH_FPS = 30;
-}
