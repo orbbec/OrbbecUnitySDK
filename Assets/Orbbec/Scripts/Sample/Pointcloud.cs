c164708563bb7029f1c86f1fd9985155c5953ac7
wuhao <wuhao@orbbec.com>
2022-11-04 18:26:53 +0800

    [add] add pointcloud sample

---
+++ Assets/Orbbec/Scripts/Sample/Pointcloud.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Orbbec;
+using OrbbecUnity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pointcloud : MonoBehaviour
+{
+    public OrbbecPipeline pipeline;
+    public Button pointcloudButton;
+    public Button colorPointcloudButton;
+
+    private PointCloudFilter filter;
+    private Format format;
+    private bool save;
+    private byte[] data;
+
+    private string pointcloudPath;
+    private string colorPointcloudPath;
+
+    void Start()
+    {
+        pointcloudPath = Application.persistentDataPath + "/points.ply";
+        colorPointcloudPath = Application.persistentDataPath + "/color_points.ply";
+
+        pointcloudButton.onClick.AddListener(SavePointcloud);
+        colorPointcloudButton.onClick.AddListener(SaveColorPointcloud);
+
+        pipeline.SetFramesetCallback(OnFrameset);
+        pipeline.onPipelineInit.AddListener(()=>{
+            pipeline.Config.SetAlignMode(AlignMode.ALIGN_D2C_HW_MODE);
+            pipeline.StartPipeline();
+            filter = new PointCloudFilter();
+            filter.SetCameraParam(pipeline.Pipeline.GetCameraParam());
+        });
+    }
+
+    private void OnFrameset(Frameset frameset)
+    {
+        filter.SetPointFormat(format);
+
+        var frame = filter.Process(frameset);
+        if(frame != null)
+        {
+            var pointFrame = frame.As<PointsFrame>();
+            var dataSize = pointFrame.GetDataSize();
+            if(data == null || data.Length != dataSize)
+            {
+                data = new byte[dataSize];
+            }
+            pointFrame.CopyData(ref data);
+            pointFrame.Dispose();
+            frame.Dispose();
+        }
+
+        frameset.Dispose();
+
+        if(save)
+        {
+            if(format == Format.OB_FORMAT_POINT)
+            {
+                WritePointPly();
+            }
+            else if(format == Format.OB_FORMAT_RGB_POINT)
+            {
+                WriteColorPointPly();
+            }
+        }
+    }
+
+    private void SavePointcloud()
+    {
+        format = Format.OB_FORMAT_POINT;
+        save = true;
+    }
+
+    private void SaveColorPointcloud()
+    {
+        format = Format.OB_FORMAT_RGB_POINT;
+        save = true;
+    }
+
+    private void WritePointPly()
+    {
+        int pointSize = Marshal.SizeOf(typeof(Point));
+        int pointsSize = data.Length / Marshal.SizeOf(typeof(Point));
+
+        Point[] points = new Point[pointsSize];
+        
+        IntPtr dataPtr = Marshal.AllocHGlobal(data.Length);
+        Marshal.Copy(data, 0, dataPtr, data.Length);
+        for(int i = 0; i < pointsSize; i++)
+        {
+            IntPtr pointPtr = new IntPtr(dataPtr.ToInt64() + i * pointSize);
+            points[i] = Marshal.PtrToStructure<Point>(pointPtr);
+        }
+        Marshal.FreeHGlobal(dataPtr);
+
+        FileStream fs = new FileStream(pointcloudPath, FileMode.Create);
+        var writer = new StreamWriter(fs);
+        writer.Write("ply\n");
+        writer.Write("format ascii 1.0\n");
+        writer.Write("element vertex " + pointsSize + "\n");
+        writer.Write("property float x\n");
+        writer.Write("property float y\n");
+        writer.Write("property float z\n");
+        writer.Write("end_header\n");
+
+        for (int i = 0; i < points.Length; i ++) 
+        {
+            writer.Write(points[i].x);
+            writer.Write(" ");
+            writer.Write(points[i].y);
+            writer.Write(" ");
+            writer.Write(points[i].z);
+            writer.Write("\n");
+        }
+        
+        writer.Close();
+        fs.Close();
+
+        save = false;
+    }
+
+    private void WriteColorPointPly()
+    {
+        int pointSize = Marshal.SizeOf(typeof(ColorPoint));
+        int pointsSize = data.Length / Marshal.SizeOf(typeof(ColorPoint));
+
+        ColorPoint[] points = new ColorPoint[pointsSize];
+        
+        IntPtr dataPtr = Marshal.AllocHGlobal(data.Length);
+        Marshal.Copy(data, 0, dataPtr, data.Length);
+        for(int i = 0; i < pointsSize; i++)
+        {
+            IntPtr pointPtr = new IntPtr(dataPtr.ToInt64() + i * pointSize);
+            points[i] = Marshal.PtrToStructure<ColorPoint>(pointPtr);
+        }
+        Marshal.FreeHGlobal(dataPtr);
+
+        FileStream fs = new FileStream(colorPointcloudPath, FileMode.Create);
+        var writer = new StreamWriter(fs);
+        writer.Write("ply\n");
+        writer.Write("format ascii 1.0\n");
+        writer.Write("element vertex " + pointsSize + "\n");
+        writer.Write("property float x\n");
+        writer.Write("property float y\n");
+        writer.Write("property float z\n");
+        writer.Write("property uchar red\n");
+        writer.Write("property uchar green\n");
+        writer.Write("property uchar blue\n");
+        writer.Write("end_header\n");
+
+        for (int i = 0; i < points.Length; i ++) 
+        {
+            writer.Write(points[i].x);
+            writer.Write(" ");
+            writer.Write(points[i].y);
+            writer.Write(" ");
+            writer.Write(points[i].z);
+            writer.Write(" ");
+            writer.Write(points[i].r);
+            writer.Write(" ");
+            writer.Write(points[i].g);
+            writer.Write(" ");
+            writer.Write(points[i].b);
+            writer.Write("\n");
+        }
+        
+        writer.Close();
+        fs.Close();
+
+        save = false;
+    }
+}
